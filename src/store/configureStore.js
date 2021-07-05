b51adc72a11385744012a581f230c424ed32dacc
Chinmayee <chinmayees969@gmail.com>
2021-07-05 10:01:43 +0530

    geolocation added

---
+++ src/store/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from 'redux';
+import locationsReducer from './reducers/locations';
+const rootReducer = combineReducers({
+    locationsList: locationsReducer
+});
+const configureStore = () => {
+    return createStore(rootReducer);
+};
+export default configureStore;
